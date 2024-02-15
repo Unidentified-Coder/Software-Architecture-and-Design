173cbedf659c99acff2fcc29d57b3f066b04c18a
Rodas Samson Gebrtensea (Student) <gebrtenr@roehampton.ac.uk>
2024-02-15 14:29:59 +0000

    nice

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Dog myDog = new Dog();
 
-        myDog.sound();
+        x = 5
+
+        if(x = 5);
+            myDog.sound();
+        else;
+            System.out.println("No Dogs are available!")
     }
 }
