e9470c32ea4999252d949328b502e8c9a38064e7
Kaue <Kaue.ravagnani@outlook.com>
2024-02-15 14:37:36 +0000

    e

--- Main.java
+++ Main.java
@@ -2,11 +2,18 @@ public class Main {
     public static void main(String[] args) {
         Dog myDog = new Dog();
 
-        int x = 5
+        int x = 9;
 
-        if(x = 5);
+        if(x == 5){
+
+            
             myDog.sound();
-        else;
-            System.out.println("No Dogs are available!")
+        }
+        
+        else
+        {
+
+            System.out.println("No Dogs are available!");
+        }
     }
 }
