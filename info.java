328e6eb25151bb6eba4d4c3806ab8ef39be33c07
Rodas Samson Gebrtensea <158178400+Unidentified-Coder@users.noreply.github.com>
2024-02-16 09:48:52 +0000

    Step 1

---
+++ info.java
@@ -0,0 +1,7 @@
+// Interface
+public interface info {
+    // Within the interface they don't have body's 
+    public void scoutInfo();
+    public void placePointer();
+    public void compareTopPrice();
+}
