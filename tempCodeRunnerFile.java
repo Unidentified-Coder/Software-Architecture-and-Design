be5ffb0a7a874e89f79c96c507da15684e2dbf80
Rodas Samson Gebrtensea <158178400+Unidentified-Coder@users.noreply.github.com>
2024-02-20 23:35:20 +0000

    New code with errors!

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+        // System.out.println("Passing infomation accross");
---
+++ viewInfo.java
@@ -0,0 +1,11 @@
+class viewInfo implements info {
+    public void scoutInfo(){
+        System.out.println("Starting up");
+
+        // Thread.sleep(22);
+
+        // System.out.println("Passing infomation accross");
+    }
+
+
+}
