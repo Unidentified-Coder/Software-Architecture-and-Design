cc54774195a86ed6046beb5a20f4d3f97530d74e
Rodas Samson Gebrtensea <gebrtenr@roehampton.ac.uk>
2024-03-07 17:20:07 +0000

    New classes

---
+++ Code/Company.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+class Company implements Server {
+    
+    public void StockExchange(){
+        System.out.println("Currently the stock exchange is £1 ∼ $3.545");
+        
+    }
+
+    public void Share(){
+        System.out.println("Currently Share Price is ");
+        System.out.println("1 Share = £0.3004");
+
+        System.out.println("Currently user " + User);
+        System.out.println("Has 5 Shares Valued at £50");
+
+    }
+
+    public void Index(){
+        System.out.println("Currently the stocks that are up are : Google\\n");
+    }
+    
+}
