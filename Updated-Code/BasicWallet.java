0f85a8ee6ea6ce1a953cc6fab7ddc483f0177c54
Kaue <Kaue.ravagnani@outlook.com>
2024-03-13 17:44:04 +0000

    code

---
+++ Updated-Code/BasicWallet.java
@@ -0,0 +1,9 @@
+public class BasicWallet implements Wallet {
+    public void deposit() {
+        System.out.println("Deposit successful.");
+    }
+
+    public void withdraw() {
+        System.out.println("Withdrawal successful.");
+    }
+}
