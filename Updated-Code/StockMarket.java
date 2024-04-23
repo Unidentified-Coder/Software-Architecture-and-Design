e999800f9a9f225292f88b32b9a292a672a6b937
Rodas Samson Gebrtensea <rsamson139@gmail.com>
2024-04-24 00:19:31 +0100

    updated stock market with integrated user selection

--- Updated-Code/StockMarket.java
+++ Updated-Code/StockMarket.java
@@ -1,15 +1,65 @@
 import java.util.Scanner;
 
-class StockMarket implements StockExchange{
+interface StockExchange {
+    void displayStockInfo();
+}
 
-    public void SharePrice(){
-        
-        System.out.println("Currently Share Price is ");
-        System.out.println("Has 5 Shares Valued at £50");
+class StockMarket implements StockExchange {
+    private Scanner scanner;
+
+    public StockMarket() {
+        scanner = new Scanner(System.in);
+        int number = 0; // Initialize variable
+
+        try {
+            System.out.println("Please enter a number from 1-4:");
+            number = scanner.nextInt();
+            System.out.println("You entered: " + number);
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+        } finally {
+            scanner.close(); // Always close the scanner
+        }
+
+        displayStockInfo(number);
     }
 
-    public void StockData(){
+    public void displayStockInfo(int number) {
+        // The 'number' variable is now accessible here
+        switch (number) {
+            case 1:
+                System.out.println("Google Stocks");
+                break;
+            case 2:
+                System.out.println("Apple Stocks");
+                break;
+            case 3:
+                System.out.println("Samsung Stocks");
+                break;
+            case 4:
+                System.out.println("Tesla Stocks");
+                break;
+            default:
+                // Handle case where number is not between 1 and 4
+                if (number < 1 || number > 4) {
+                    System.out.println("Number is not within the valid range (1-4).");
+                }
+                break;
+        }
+    }
 
-        System.out.println("Currently the stocks that are up are : Google\\n");
+    public static void main(String[] args) {
+        new StockMarket();
     }
 }
+
+// public void SharePrice(){
+    
+//     System.out.println("Currently Share Price is ");
+//     System.out.println("Has 5 Shares Valued at £50");
+// }
+
+// public void StockData(){
+
+//     System.out.println("Currently the stocks that are up are : Google\\n");
+// }
